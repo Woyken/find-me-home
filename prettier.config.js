45325f492730a27592f8929763156bfc1b5d2f2c
Karolis Užkuraitis <13166802+Woyken@users.noreply.github.com>
2026-09-05 15:40:30 +0300

    Add throwaway bookmarklet loader probe prototype under Pages

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,13 @@ const config = {
   semi: false,
   singleQuote: true,
   trailingComma: "all",
+  overrides: [
+    {
+      // Prototype code that gets flattened into javascript: URLs must not rely on ASI.
+      files: ["public/prototype/**"],
+      options: { semi: true },
+    },
+  ],
 };
 
 export default config;
